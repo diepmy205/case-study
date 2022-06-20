65caabc695fb8767cd9d27855c7d8657b634f56e
unknown <huangyemei205@gmail.com>
2022-06-20 15:08:42 +0700

    upload

---
+++ game.js
@@ -0,0 +1,211 @@
+const pointForEach = 10
+const maxQuiz = 10
+
+let availableQuiz = []
+let currentQuiz = {}
+let quizCount
+let score
+let numOfCorrectAns
+let wrongAnswerQuiz = []
+
+const startBox = document.getElementById("startBox")
+const instruction = document.getElementById("instruction")
+const gameBox = document.getElementById("gameBox")
+const endGameBox = document.getElementById("endGameBox")
+const optionsContainer = document.getElementById("optionsContainer")
+const nextButton = document.getElementById("nextButton")
+const firework = document.getElementById("firework")
+const wrongAnswerBox = document.getElementById("wrongAnswerBox")
+
+let correctSound = new Audio('/sounds/correct-answer.mp3')
+let wrongSound = new Audio('/sounds/wrong-answer.mp3')
+let whooshSound = new Audio('/sounds/whoosh3.mp3')
+let endSound = new Audio('/sounds/end game.mp3')
+let clickSound = new Audio('/sounds/mouse-click.mp3')
+let perfectScoreSound = new Audio('/sounds/success.mp3')
+
+document.getElementById("instructionText").innerHTML = "Bạn sẽ phải trả lời " + maxQuiz + " câu đố ngẫu nhiên về những kiến thức liên quan đến Nhật Bản<br>" +
+    "Mỗi câu chỉ có duy nhất 1 đáp án đúng, hãy click chuột vào đáp án mà bạn muốn chọn<br>" +
+    "Đáp án đúng sẽ hiện màu xanh, đáp án sai sẽ hiện màu đỏ<br>" +
+    "Mỗi câu có giá trị là " + pointForEach + " điểm, tổng số điểm là " + pointForEach * maxQuiz + "<br>" +
+    "Hãy thử xem bạn hiểu bao nhiêu về Nhật Bản nhé!"
+
+
+function showInstruction() {
+    instruction.classList.remove("hide")
+    clickSound.play()
+}
+
+function hideInstruction() {
+    instruction.classList.add("hide")
+    clickSound.play()
+}
+
+function startGame() {
+    startBox.classList.add("hide")
+    gameBox.classList.remove("hide")
+    quizCount = 0
+    score = 0
+    numOfCorrectAns = 0
+    availableQuiz = [...quizList]
+    setTimeout(function() { getNewQuiz() }, 150)
+    clickSound.play()
+    progressMark()
+}
+
+function progressMark() {
+    for (let i = 0; i < maxQuiz; i++) {
+        const mark = document.createElement("div")
+        mark.className = "mark"
+        document.getElementById("progressMark").appendChild(mark)
+    }
+}
+
+function getNewQuiz() {
+    quizCount++
+    progress()
+    randomQuiz()
+    nextButton.style.visibility = "hidden"
+    setTimeout(function() { whooshSound.play() }, 50)
+
+    if (quizCount === maxQuiz)
+        nextButton.innerText = 'Kết thúc quiz'
+}
+
+function progress() {
+    document.getElementById("progressText").innerHTML = "Câu thứ " +
+        "<span style='color: rgb(37, 155, 187); font-weight: 800; font-size: 1.2rem; text-shadow: 0.1rem 0.1rem 0.5rem rgb(184, 219, 229)'>" +
+        quizCount + "</span>" +
+        "  trên " +
+        "<span style='color: rgb(37, 155, 187); font-weight: 600;text-shadow: 0.1rem 0.1rem 0.5rem rgb(184, 219, 229)'>" +
+        maxQuiz + "</span>" + " câu"
+    document.getElementById("progressMark").children[quizCount - 1].classList.add("answering")
+}
+
+function randomQuiz() {
+    let quizIndex = Math.floor(Math.random() * availableQuiz.length)
+    currentQuiz = availableQuiz[quizIndex]
+    document.getElementById("question").innerHTML = currentQuiz.question
+    availableQuiz.splice(quizIndex, 1)
+    optionsContainer.innerHTML = ''
+
+    let availableOptions = []
+    for (let i = 0; i < currentQuiz.options.length; i++) {
+        availableOptions.push(i)
+    }
+
+    for (let i = 0; i < currentQuiz.options.length; i++) {
+        let optionIndex = Math.floor(Math.random() * availableOptions.length)
+        const option = document.createElement("div")
+        option.innerHTML = currentQuiz.options[availableOptions[optionIndex]]
+        option.className = "option"
+        option.id = availableOptions[optionIndex]
+        option.setAttribute("onclick", "getResult(this)")
+        optionsContainer.appendChild(option)
+        availableOptions.splice(optionIndex, 1)
+    }
+}
+
+
+function getResult(clickedOption) {
+    notAcceptingAnswer()
+    if (clickedOption.innerHTML === currentQuiz.options[currentQuiz.answer]) {
+        clickedOption.classList.add("correct", "clickedOption")
+        addScore()
+        setTimeout(function() { correctSound.play() }, 900)
+        setTimeout(function() { document.getElementById("progressMark").children[quizCount - 1].classList.add("correctMark") }, 1000)
+    } else {
+        clickedOption.classList.add("wrong", "clickedOption")
+        document.getElementById(currentQuiz.answer).classList.add("correct")
+        addwrongQuiz(clickedOption)
+        setTimeout(function() { wrongSound.play() }, 900)
+        setTimeout(function() { document.getElementById("progressMark").children[quizCount - 1].classList.add("wrongMark") }, 1000)
+    }
+}
+
+function addwrongQuiz(wrong) {
+    let wrongQuiz = document.createElement("div")
+    wrongQuiz.innerHTML = "Câu " + quizCount + ": " + currentQuiz.question
+    wrongQuiz.className = "wrongQuestion"
+    document.getElementById("wrongAnswerQuiz").appendChild(wrongQuiz)
+
+    let wrongQuizAnswer = document.createElement("div")
+    wrongQuizAnswer.innerHTML = "Câu trả lời của bạn: " +
+        "<span style='color:red; font-weight:500'>" + wrong.innerHTML + "</span>" + "<br>" +
+        "Câu trả lời đúng: " +
+        "<span style='color:green; font-weight:600'>" + currentQuiz.options[currentQuiz.answer] + "</span>"
+    wrongQuizAnswer.className = "wrongAnswer"
+    document.getElementById("wrongAnswerQuiz").appendChild(wrongQuizAnswer)
+}
+
+function notAcceptingAnswer() {
+    for (let i = 0; i < optionsContainer.children.length; i++) {
+        optionsContainer.children[i].classList.add("notAcceptingAnswer")
+    }
+    setTimeout(function() { nextButton.style.visibility = "visible" }, 1700)
+}
+
+function addScore() {
+    numOfCorrectAns++
+    score += pointForEach
+    setTimeout(function() { document.getElementById("score").innerHTML = score }, 1200)
+}
+
+function next() {
+    if (quizCount < maxQuiz)
+        getNewQuiz()
+    else {
+        clickSound.play()
+        setTimeout(function() { endGame() }, 500)
+    }
+}
+
+function endGame() {
+    gameBox.classList.add("hide")
+    endGameBox.classList.remove("hide")
+    if (score == maxQuiz * pointForEach) {
+        setTimeout(function() { perfectScoreSound.play() }, 300)
+        setTimeout(function() { firework.classList.remove("hide") }, 300)
+        if (document.getElementById("reviewButton").style.display != "hide")
+            document.getElementById("reviewButton").classList.add("hide")
+    } else {
+        setTimeout(function() { endSound.play() }, 300)
+        document.getElementById("reviewButton").classList.remove("hide")
+    }
+    showLastScore()
+}
+
+function showLastScore() {
+    document.getElementById("lastScore").innerHTML = "Số điểm của bạn: " +
+        "<span style='color: rgb(37, 155, 187); font-weight: 600; font-size: 1.5rem; text-shadow: 0.1rem 0.1rem 0.5rem rgb(184, 219, 229)'>" +
+        score + "</span>"
+    document.getElementById("lastCorrectAnswer").innerHTML = "Số câu trả lời đúng: " +
+        "<span style='color: rgb(37, 155, 187); font-weight: 600; font-size: 1.5rem; text-shadow: 0.1rem 0.1rem 0.5rem rgb(184, 219, 229)'>" +
+        numOfCorrectAns + "/" + maxQuiz + "</span>"
+}
+
+function showWrongAnswerBox() {
+    endGameBox.classList.add("hide")
+    wrongAnswerBox.classList.remove("hide")
+    clickSound.play()
+}
+
+function hideWrongAnswerBox() {
+    wrongAnswerBox.classList.add("hide")
+    endGameBox.classList.remove("hide")
+    clickSound.play()
+}
+
+function restart() {
+    endGameBox.classList.add("hide")
+    startBox.classList.remove("hide")
+    document.getElementById("score").innerHTML = "0"
+    nextButton.innerText = 'Câu tiếp theo'
+    document.getElementById("wrongAnswerQuiz").innerHTML = ''
+    setTimeout(function() { clickSound.play() }, 50)
+    if (instruction.style.display != "none")
+        hideInstruction()
+    if (firework.style.display != "none")
+        firework.classList.add("hide")
+    document.getElementById("progressMark").innerHTML = ''
+}
---
+++ quiz.js
@@ -0,0 +1,31 @@
+class quiz {
+    question
+    options
+    answer
+
+    constructor(question, options, answer) {
+        this.question = question
+        this.options = options
+        this.answer = answer
+    }
+}
+
+let quizList = []
+quizList.push(new quiz('Đâu là tỉnh có dân số đông nhất Nhật Bản?', ['Tokyo', 'Osaka', 'Saitama', 'Kyoto'], '0'))
+quizList.push(new quiz('Núi Phú Sĩ cao bao nhiêu?', ['3776m', '3767m', '3677m', '3676m'], '0'))
+quizList.push(new quiz('Ai là người được in hình trên tờ 10.000 yên?', ['Fukuzawa Yukichi', 'Natsume Soseki', 'Higuchi Ichio', 'Noguchi Hideo'], '0'))
+quizList.push(new quiz('Ngoài hoa anh đào, đâu là quốc hoa của Nhật Bản?', ['Hoa cúc', 'Hoa cẩm tú cầu', 'Hoa tử đằng', 'Hoa diên vỹ', 'Hoa bồ công anh'], '0'))
+quizList.push(new quiz('Ở Nhật, bao nhiêu tuổi được tính là trưởng thành?', ['20', '18', '16'], '0'))
+quizList.push(new quiz('Người Nhật thờ cúng và đón tổ tiên về nhà vào dịp lễ nào trong năm?', ['Lễ Obon', 'Năm mới', 'Tuần lễ vàng', 'Lễ tiết phân'], '0'))
+quizList.push(new quiz('Môn bắn cung của Nhật Bản được gọi là gì?', ['Kyudo', 'Jyudo', 'Shodo', 'Kendo'], '0'))
+quizList.push(new quiz('Onigiri là tên gọi của món ăn gì?', ['Cơm nắm', 'Chả cá viên', 'Bánh rán', 'Đậu tương lên men'], '0'))
+quizList.push(new quiz('Ở Nhật, con số nào được coi là xui xẻo?', ['4', '13', '6', '8'], '0'))
+quizList.push(new quiz('Trường học ở Nhật thường khai giảng vào tháng mấy?', ['Tháng 4', 'Tháng 1', 'Tháng 9', 'Tháng 11'], '0'))
+quizList.push(new quiz('Tỉnh nào sau đây giáp với Tokyo?', ['Kanagawa', 'Hokkaido', 'Hiroshima', 'Shizuoka'], '0'))
+quizList.push(new quiz('Thủ tướng hiện tại của Nhật Bản tên là gì?', ['Kishida Fumio', 'Abe Shinzo', 'Suga Yoshihide', 'Noda Yoshihiko'], '0'))
+quizList.push(new quiz('Ở Nhật người ta lái xe ở đường bên nào?', ['Bên trái', 'Bên phải'], '0'))
+quizList.push(new quiz('Tác giả của bộ truyện "Thám tử lừng danh Conan" là ai?', ['Aoyama Gosho', 'Tezuka Osamu', 'CLAMP', 'Fujiko F.Fujio'], '0'))
+quizList.push(new quiz('Đâu là bộ phim có doanh thu phòng vé cao nhất trong lịch sử ở Nhật?', ['Thanh gươm diệt quỷ: Chuyến tàu vô tận', 'Vùng đất linh hồn', 'Tên cậu là gì?'], '0'))
+quizList.push(new quiz('Tỉnh nào có diện tích nhỏ nhất Nhật Bản?', ['Kagawa', 'Tokyo', 'Saga', 'Kanagawa'], '0'))
+quizList.push(new quiz('Tỉnh nào có diện tích lớn nhất Nhật Bản?', ['Hokkaido', 'Niigata', 'Okinawa', 'Nagano'], '0'))
+quizList.push(new quiz('Bảng chữ Hiragana được sử dụng ngày nay có bao nhiêu chữ cái?', ['46', '44', '48', '50'], '0'))
